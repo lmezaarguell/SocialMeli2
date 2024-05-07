5f5b43fd0f3c0af8f91b082ba820eb2b6387fdee
nico-imbachi-meli <johan.imbachi@mercadolibre.com.co>
2024-05-07 15:57:21 -0500

    Added Unit Test for Buyer Respository

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/repository/BuyerRepositoryTest.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/repository/BuyerRepositoryTest.java
@@ -1,4 +1,30 @@
 package com.meli.be_java_hisp_w26_g10.repository;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.repository.impl.BuyerRepositoryImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BuyerRepositoryTest {
+
+    BuyerRepositoryImpl buyerRepository = new BuyerRepositoryImpl();
+
+    @Test
+    @DisplayName("Obtener un usuario que existe ")
+    public void getAnExistingUser(){
+        //Act && Arrange
+        Buyer buyerObtained = buyerRepository.getById(1);
+        //Assert
+        Assertions.assertNotNull(buyerObtained);
+    }
+
+    @Test
+    @DisplayName("Obtener un usuario que no existe ")
+    public void getANonExistingUser(){
+        //Act && Arrange
+        Buyer buyerObtained = buyerRepository.getById(100);
+        //Assert
+        Assertions.assertNull(buyerObtained);
+    }
 }
