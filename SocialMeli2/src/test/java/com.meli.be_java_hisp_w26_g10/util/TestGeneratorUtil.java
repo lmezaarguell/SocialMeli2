beab5ece8f16a3337b5c1b56e1446ae0ba6a8207
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-05-07 08:54:41 -0500

    define the utils methods for the US004 and US008

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
@@ -11,5 +11,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestGeneratorUtil {
+    private static List<Buyer> buyers = loadData();
+    private static List<Buyer> loadData(){
+        List<Buyer> buyers = new ArrayList<>();
+        String route = "classpath:buyer.json";
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file;
+        try {
+            file = ResourceUtils.getFile(route);
 
+            Buyer[] buyersArray = objectMapper.readValue(file, Buyer[].class);
+
+            for (Buyer b : buyersArray) {
+                buyers.add(b);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buyers;
+    }
+
+    public static Buyer getBuyerById(Integer id) {
+        Buyer buyer = buyers.stream().filter(
+                b -> b.getUser_id().equals(id)).findFirst().orElse(null);
+        //Valida que sea un usario registrado y retorna el cliente
+        if (buyer == null) throw new NotFoundException("El usuario no existe o no se encuentra registrado.");
+        return buyer;
+    }
 }
