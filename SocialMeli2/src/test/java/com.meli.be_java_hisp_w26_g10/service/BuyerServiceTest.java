409ec91aadb43c06ab343291c485ad9a54b14c17
lmezaarguell <luisfernando.mezaarguello@mercadolibre.com.mx>
2024-05-07 14:58:33 -0600

    Adding coments to SellerServiceImpl, adding test with happy path and adding a static method to Test Util to have one Buyer

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/BuyerServiceTest.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/BuyerServiceTest.java
@@ -1,4 +1,50 @@
 package com.meli.be_java_hisp_w26_g10.service;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.entity.Seller;
+import com.api.socialmeli.exception.NotFoundException;
+import com.api.socialmeli.repository.impl.BuyerRepositoryImpl;
+import com.api.socialmeli.service.impl.BuyerServiceImpl;
+import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
 public class BuyerServiceTest {
+
+    @Mock
+    BuyerRepositoryImpl repository;
+
+    @InjectMocks
+    BuyerServiceImpl service;
+
+    @Test
+    @DisplayName("Realizar la accion de dejar de seguir a un vendedor de forma exitosa")
+    public void unfollowUserGoodWay() {
+
+        Integer followerId = 1;
+        Integer unfollowId = 1;
+
+        Buyer buyerExpected = TestGeneratorUtil.getSingleBuyer();
+
+        when(repository.getById(followerId)).thenReturn(buyerExpected);
+
+        service.unfollowUser(followerId, unfollowId);
+
+        verify(repository, times(2)).getById(followerId);
+
+    }
+
+
+
+
 }
--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
@@ -1,8 +1,23 @@
 package com.meli.be_java_hisp_w26_g10.util;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.entity.Seller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestGeneratorUtil {
 
+    public static Buyer getSingleBuyer() {
+        List<Seller> sellerList = new ArrayList<>();
+        Seller seller1 = new Seller(1, "Luis");
 
+        sellerList.add(seller1);
+
+        Buyer myBuyer = new Buyer(1, "Luis", sellerList);
+        return myBuyer;
+    }
 
     public static String returnAString() {
         return "Hola";
