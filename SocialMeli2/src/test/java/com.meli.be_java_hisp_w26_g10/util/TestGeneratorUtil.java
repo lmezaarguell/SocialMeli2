568a5fe4ad3125f5b1776c934a675d98175a0fd8
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-05-07 08:44:48 -0500

    Created the buyer environment inside testUtilGenerator class

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
@@ -1,10 +1,20 @@
 package com.meli.be_java_hisp_w26_g10.util;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.entity.Seller;
+
+import java.util.List;
+
 public class TestGeneratorUtil {
 
 
 
     public static String returnAString() {
         return "Hola";
+    }
+    public static List<Buyer> createBuyerEnvironment(){
+        Buyer buyerOne = new Buyer(1,"Miguel Guzman",List.of(new Seller(1,"Meli"),new Seller(2,"Adidas"),new Seller(3,"Levis")));
+        Buyer buyerTwo = new Buyer(2,"Juan Perez",List.of(new Seller(1,"Meli")));
+        return List.of(buyerOne,buyerTwo);
     }
 }
