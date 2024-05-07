8e746ff6c336a0c47133ef2e621d9caeffdd7829
juagonzalezf <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-05-07 08:13:04 -0600

    test(us-003): US-003 covered three test, oder: asc, desc, no order

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/SellerServiceTest.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/SellerServiceTest.java
@@ -1,4 +1,117 @@
 package com.meli.be_java_hisp_w26_g10.service;
 
+import com.api.socialmeli.dto.FollowedBySellerDto;
+import com.api.socialmeli.dto.UserDto;
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.entity.Seller;
+import com.api.socialmeli.exception.BadRequestException;
+import com.api.socialmeli.repository.IBuyerRepository;
+import com.api.socialmeli.repository.ISellerRepository;
+import com.api.socialmeli.service.impl.SellerServiceImpl;
+import com.api.socialmeli.utils.UserDtoShort;
+import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class SellerServiceTest {
+    @Mock
+    ISellerRepository sellerRepository;
+
+    @Mock
+    IBuyerRepository buyerRepository;
+
+
+    @InjectMocks
+    SellerServiceImpl sellerService;
+
+    @Test
+    @DisplayName("Deberia de retornar la lista de seguidores ordenado de manera ascendente por nombre")
+    public void correctAlphabeticOrderOfFollowwerASC(){
+
+        // arrange
+        String order = "name_asc";
+        Seller seller = new Seller(1, "Meli");
+        List<Buyer> buyers = TestGeneratorUtil.generateBuyerList();
+
+        /* Creacion de followers y followers ordenados  */
+        List<UserDto> followers = TestGeneratorUtil.generateUserDtoList();
+        List<UserDto> sortedUsers = UserDtoShort.sortList(followers, order);
+
+        /* Creacion de la salida del servicio */
+        FollowedBySellerDto expectedFollowedBySellerDto = new FollowedBySellerDto(
+                seller.getUser_id(),
+                seller.getUser_name(),
+                followers
+        );
+
+        // act
+        when(sellerRepository.getById(anyInt())).thenReturn(seller);
+        when(buyerRepository.getAll()).thenReturn(buyers);
+        MockedStatic<UserDtoShort> userDtoShort = mockStatic(UserDtoShort.class);
+        userDtoShort.when(() -> UserDtoShort.sortList(any(List.class), anyString())).thenReturn(sortedUsers);
+        FollowedBySellerDto actualFollowedBySellerDto = sellerService.getFollowersOfSeller(1, order);
+
+        // assert
+        assertFalse(actualFollowedBySellerDto.getFollowers().isEmpty());
+        assertEquals(sortedUsers, actualFollowedBySellerDto.getFollowers());
+
+        userDtoShort.close();
+    }
+
+    @Test
+    @DisplayName("Deberia de retornar la lista de seguidores ordenado de manera descendente por nombre")
+    public void correctAlphabeticOrderOfFollowwerDESC(){
+        // arrange
+        String order = "name_desc";
+        Seller seller = new Seller(1, "Meli");
+        List<Buyer> buyers = TestGeneratorUtil.generateBuyerList();
+
+        /* Creacion de followers y followers ordenados  */
+        List<UserDto> followers = TestGeneratorUtil.generateUserDtoList();
+        List<UserDto> sortedUsers = UserDtoShort.sortList(followers, order);
+
+        /* Creacion de la salida del servicio */
+        FollowedBySellerDto expectedFollowedBySellerDto = new FollowedBySellerDto(
+                seller.getUser_id(),
+                seller.getUser_name(),
+                followers
+        );
+
+        // act
+        when(sellerRepository.getById(anyInt())).thenReturn(seller);
+        when(buyerRepository.getAll()).thenReturn(buyers);
+        MockedStatic<UserDtoShort> userDtoShort = mockStatic(UserDtoShort.class);
+        userDtoShort.when(() -> UserDtoShort.sortList(any(List.class), anyString())).thenReturn(sortedUsers);
+        FollowedBySellerDto actualFollowedBySellerDto = sellerService.getFollowersOfSeller(1, order);
+
+        // assert
+        assertFalse(actualFollowedBySellerDto.getFollowers().isEmpty());
+        assertEquals(sortedUsers, actualFollowedBySellerDto.getFollowers());
+
+        userDtoShort.close();
+    }
+
+    @Test
+    @DisplayName("Deberia de retornar una excepcion con mensaje ordenamiento no existente")
+    public void incorrectAlphabeticOrderFollower(){
+        // arrange
+        String order = "name_desc";
+        Seller seller = new Seller(1, "Meli");
+        assertThrows(BadRequestException.class,
+                () -> sellerService.getFollowersOfSeller(seller.getUser_id(), "any order")
+        );
+    }
 }
--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
@@ -1,10 +1,52 @@
 package com.meli.be_java_hisp_w26_g10.util;
 
+import com.api.socialmeli.dto.UserDto;
+import com.api.socialmeli.entity.Buyer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestGeneratorUtil {
 
 
 
     public static String returnAString() {
         return "Hola";
+    }
+
+    public static List<Buyer> generateBuyerList(){
+        return new ArrayList<>(
+                List.of(
+                        new Buyer(1, "Antonio", Collections.emptyList()),
+                        new Buyer(2, "Cesar", Collections.emptyList()),
+                        new Buyer(3, "Jorge", Collections.emptyList()),
+                        new Buyer(4, "Octavio", Collections.emptyList()),
+                        new Buyer(5, "Zapata", Collections.emptyList())
+                )
+        );
+    }
+
+    public static List<UserDto> generateUserDtoList(){
+        return new ArrayList<>(
+                List.of(
+                        new UserDto(
+                                3,
+                                "Zapata"
+                        ),
+                        new UserDto(
+                                1,
+                                "Antonio"
+                        ),
+                        new UserDto(
+                                2,
+                                "Octavio"
+                        ),
+                        new UserDto(
+                                4,
+                                "Cesar"
+                        )
+                )
+        );
     }
 }
