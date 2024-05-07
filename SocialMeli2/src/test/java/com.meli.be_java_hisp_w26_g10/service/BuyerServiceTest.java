e112f81e29785f6a3b09d94eceea592cc8dc8473
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-05-07 08:52:19 -0500

    add schema testing for US004

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/BuyerServiceTest.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/BuyerServiceTest.java
@@ -1,4 +1,71 @@
 package com.meli.be_java_hisp_w26_g10.service;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.repository.IBuyerRepository;
+import com.api.socialmeli.service.impl.BuyerServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class BuyerServiceTest {
+
+    @Mock
+    IBuyerRepository buyerRepository;
+
+    @InjectMocks
+    BuyerServiceImpl buyerService;
+
+
+    @Test
+    @DisplayName("Obtener la lista de seguidos por usuario de manera exitosa")
+    public void GetFollowedListByUserSuccessful(){
+
+    }
+
+    @Test
+    @DisplayName("Obtener la lista de seguidos de un usuario que no existe")
+    public void GetFollowedListByUserNotFound(){
+
+    }
+
+    @Test
+    @DisplayName("validar que funcione el ordenamiento ascendente")
+    public void ValidateOrderUpwardSuccessful(){
+
+    }
+
+    @Test
+    @DisplayName("validar que funcione el ordenamiento descendente")
+    public void ValidateOrderDownwardSuccessful(){
+
+    }
+
+    @Test
+    @DisplayName("Obtener la lista de seguidos de un usuario ordenada ascendentemente")
+    public void GetFollowedListByUserOrderUpwardSuccessful(){
+
+    }
+
+    @Test
+    @DisplayName("Obtener la lista de seguidos de un usuario ordenada descendentemente")
+    public void GetFollowedListByUserOrderDownwardSuccessful(){
+
+    }
+
+    @Test
+    @DisplayName("Obtener la lista de seguidos de un usuario con parametros de ordenamiento incorrectos")
+    public void GetFollowedListByUserOrderFailed(){
+
+    }
+
 }
--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/util/TestGeneratorUtil.java
@@ -1,10 +1,15 @@
 package com.meli.be_java_hisp_w26_g10.util;
 
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.exception.NotFoundException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestGeneratorUtil {
 
-
-
-    public static String returnAString() {
-        return "Hola";
-    }
 }
