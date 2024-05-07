df7c043500f730b084b2231c779069c4035d4657
nhoyosmeli <nicolas.hoyos@mercadolibre.com.co>
2024-05-07 15:39:02 -0500

    Some more refactoring over the tests

--- SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/SellerServiceTest.java
+++ SocialMeli2/src/test/java/com.meli.be_java_hisp_w26_g10/service/SellerServiceTest.java
@@ -3,8 +3,11 @@ package com.meli.be_java_hisp_w26_g10.service;
 import com.api.socialmeli.dto.SellersCountFollowersDto;
 import com.api.socialmeli.entity.Buyer;
 import com.api.socialmeli.entity.Seller;
+import com.api.socialmeli.exception.NotFoundException;
 import com.api.socialmeli.repository.IBuyerRepository;
 import com.api.socialmeli.repository.ISellerRepository;
+import com.api.socialmeli.repository.impl.BuyerRepositoryImpl;
+import com.api.socialmeli.repository.impl.SellerRepositoryImpl;
 import com.api.socialmeli.service.impl.SellerServiceImpl;
 import com.meli.be_java_hisp_w26_g10.util.TestGeneratorUtil;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +18,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -22,10 +26,10 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class SellerServiceTest {
     @Mock
-    IBuyerRepository buyerRepository;
+    BuyerRepositoryImpl buyerRepository;
 
     @Mock
-    ISellerRepository sellerRepository;
+    SellerRepositoryImpl sellerRepository;
 
     @InjectMocks
     SellerServiceImpl sellerService;
@@ -33,7 +37,6 @@ public class SellerServiceTest {
     @Test
     @DisplayName("Testeando un correcto escenario para contar seguidores")
     public void followerCounterOk(){
-        //Arrange
 
         List<Buyer> buyers = TestGeneratorUtil.createBuyerEnvironment();
         List<Seller> sellers = TestGeneratorUtil.createSellerEnvironment();
@@ -44,5 +47,14 @@ public class SellerServiceTest {
         SellersCountFollowersDto result = sellerService.getCountOfSellerFollowers(1);
 
         Assertions.assertEquals(2,result.getFollowers_count());
+    }
+    @Test
+    @DisplayName("Testeando el conteo de seguidores sobre un Seller sin seguidores")
+    public void followerCounterNotFound(){
+
+        List<Seller> sellers = new ArrayList<>();
+        when(sellerRepository.getAll()).thenReturn(sellers);
+
+        Assertions.assertThrows(NotFoundException.class,() -> sellerService.getCountOfSellerFollowers(0));
     }
 }
